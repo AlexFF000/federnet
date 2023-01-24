7371a61888403e8bf9d70378541938fbb207eb9b
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2023-01-24 17:32:23 +0000

    FNT-16 Add SSL support to Infrastructure Server

--- infrastructure_server/src/index.ts
+++ infrastructure_server/src/index.ts
@@ -9,11 +9,21 @@ import { KeypairSingleton } from './KeypairSingleton.js';
 import accountRoutes from './route/accountRoutes.js';
 import communityRoutes from './route/communityRoutes.js';
 import directMessageRoutes from './route/directMessageRoutes.js';
+import { readFileSync } from 'fs';
 
 // Validate environment vars
 const port: number = process.env.PORT != undefined && !isNaN(parseInt(process.env.PORT))
     ? parseInt(process.env.PORT)
     : 24401;  // Use port 24401 if none specified in env vars
+
+if (process.env.SSL_CERT_FILE === undefined || typeof process.env.SSL_CERT_FILE !== "string" || process.env.SSL_CERT_FILE === "") {
+    log.fatal("No SSL certificate file path provided in SSL_CERT_FILE environment variable");
+    process.exit(1);
+}
+if (process.env.SSL_KEY_FILE === undefined || typeof process.env.SSL_KEY_FILE !== "string" || process.env.SSL_KEY_FILE === "") {
+    log.fatal("No private key file path provided in SSL_KEY_FILE environment variable");
+    process.exit(1);
+}
 
 // Generate keypair if it doesn't already exist
 await KeypairSingleton.getInstance();
@@ -23,7 +33,12 @@ await MongoConnection.getInstance();
 // Start HTTP server
 log.info(`Starting server on port: ${port}`);
 
-const server = fastify();
+const server = fastify({
+    https: {
+        key: readFileSync(process.env.SSL_KEY_FILE),
+        cert: readFileSync(process.env.SSL_CERT_FILE)
+    }
+});
 
 server.register(accountRoutes);
 server.register(communityRoutes);
